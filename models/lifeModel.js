e7c310124886c5af44d56d322b5195fa410d1dff
qainar <kainaraskarov2002@gmail.com>
2022-06-05 19:16:52 +0600

    life

--- models/lifeModel.js
+++ models/lifeModel.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 
 const LifeModel = new mongoose.Schema({
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    IIN: {type: Number, required: true},
+    year: {type: Number, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 })
 
 export default mongoose.model('LifeModel', LifeModel)
